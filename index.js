99c7ff392a4decfb8040ace35469cf662778c1f1
vishal gawade <vishalgawade031@gmail.com>
2024-10-16 14:16:04 +0530

    the rout to show chat's

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const Chat=require("./models/chat.js");
 
-app.use("views",path.join(__dirname,"views"));
-app.use("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
 const mongoose = require('mongoose');
 
 main().then(()=>{console.log("connection succesfull")})
@@ -13,6 +16,28 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 
 }
+
+const chat1=new Chat({
+  from:"vishal",
+  to:"chetan",
+  msg:"be a fullstack",
+  created_at:new Date()
+})
+
+// chat1.save().then((res)=>{
+//   console.log(res)});
+
+
+  //index rout to featch the chat's or data
+app.get("/chats",async(req,res)=>{
+
+  let chats=await Chat.find()
+    console.log(chats);
+  res.render("index.ejs",{chats});
+  })
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("it is working");
